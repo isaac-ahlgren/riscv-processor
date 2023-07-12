c68798a2ea1edf0a0f9230b1eeb0319afcbcdbb8
isaac-ahlgren <isaacahlgren12@gmail.com>
2023-07-11 23:29:27 -0500

    added correct fetch.v

--- fetch.v
+++ fetch.v
@@ -1,15 +1,20 @@
+`timescale 1us/100ns
 
-module fetch (instr, curr_addr_step, curr_addr_addval, curr_addr_fetch, alu_bits, reg_addr, en_uncond_jmp, en_reg_jmp, 
-              en_branch, en_imm_pc_add, imm, d1, clk, rst);
+`define "stallmem.v"
+
+module fetch (instr, curr_addr_step, curr_addr_addval, curr_addr_fetch, jump_taken, alu_bits, reg_addr, en_uncond_jmp, en_reg_jmp, 
+              en_branch, en_imm_pc_add, imm, d1, data_stall, clk, rst);
    
     // Instruction from I-cache
     output wire [31:0] instr;
-    // Leaving Fetch Stage Current address plus four bytes
+    // Leaving Fetch Stage, Current address plus four bytes
     output wire [31:0] curr_addr_step_out;
-    // Leaving Fetch Stage Current address plus relative jump
+    // Leaving Fetch Stage, Current address plus relative jump
     output reg [31:0] curr_addr_addval_out;
-    // Leaving Fetch Stage Current address
-    output wire [31:0] curr_addr_out; 
+    // Leaving Fetch Stage, Current address
+    output wire [31:0] curr_addr_out;
+    // Leaving Fetch Stage, signal that shows that a jump has been taken
+    output wire jump_taken; 
 
     // From Execute, the address from the ALU
     input wire alu_bits; 
@@ -27,17 +32,47 @@ module fetch (instr, curr_addr_step, curr_addr_addval, curr_addr_fetch, alu_bits
     input wire [31:0] imm;
     // From Decode, the data to the second register
     input wire [31:0] d1;
-     // Clock and Reset 
+    // Signal that informs whether a stall is occuring because of data hazards
+    input wire data_stall; 
+    // Clock and Reset 
     input wire clk, rst;
+
+    wire ready;
+    wire err;
+
+    // Program Counter
+    dflop pc [31:0] (.q(curr_addr), .d(next_addr), .clk(clk), .rst(rst)); 
+    // Instruction Cache
+    stallmem icache (.data_out(instr), .ready(ready), .addr(curr_addr), .enable(enable), .wr(imem_wr), .createdump(createdump), .clk(clk), .rst(rst), .err(err));
 
     // Value that will be added to the current address for the relative jump
     reg [31:0] additional_val;
     assign curr_addr_plus_4bytes = curr_addr + 4;
     assign curr_addr_add_val = curr_addr + additional_val;
-    
+    assign jump_taken = en_jmp & (en_reg_jmp | en_uncond_jmp | en_branch);   
+ 
     always @(*) begin
         // Determine whether a relative jump using an immediate or using a value in a register
         if (en_imm_pc_add)
             additional_val <= imm;
         else
             additional_val <= d0;
+  
+        // Determine if a jump is to be taken
+        if (~ready) begin
+            next_addr <= curr_addr;
+        end
+        else if (en_jmp & en_reg_jmp) begin
+            next_addr <= alu_bits;
+        end
+        else if (en_jmp & (en_uncond_jmp | en_branch)) begin
+            next_addr <= curr_addr_add_val;
+        end
+        else begin
+            next_addr <= curr_addr_plus_4bytes;
+        end
+
+        if (~ready) begin
+            
+        end
+    end
